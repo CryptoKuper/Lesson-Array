33f6da2a873480f61f864c959d8afa5711d0034f
Rafael <r.tukanov1981@gmail.com>
2023-12-09 12:27:03 +0600

    Добавил решение

---
+++ Задача 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Пример двумерного массива
+        int[,] array = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        Console.Write("Введите номер строки: ");
+        int row = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите номер столбца: ");
+        int col = int.Parse(Console.ReadLine());
+
+        int result = GetArrayElement(array, row, col);
+
+        if (result != int.MinValue)
+        {
+            Console.WriteLine($"Значение элемента: {result}");
+        }
+        else
+        {
+            Console.WriteLine("Элемент с такими координатами отсутствует в массиве.");
+        }
+    }
+
+    static int GetArrayElement(int[,] array, int row, int col)
+    {
+        int rowCount = array.GetLength(0);
+        int colCount = array.GetLength(1);
+
+        if (row >= 0 && row < rowCount && col >= 0 && col < colCount)
+        {
+            return array[row, col];
+        }
+
+        // Возвращаем int.MinValue, чтобы показать, что элемент не найден
+        return int.MinValue;
+    }
+}
